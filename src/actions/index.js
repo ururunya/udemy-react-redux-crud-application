c5245747c3a067d1c954d62060e6237fa1727a49
ururunya <ururunya@gmail.com>
2021-03-13 00:09:10 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+// アクションクリエーター
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const increment = () => ({
+  type: INCREMENT
+});
+
+export const decrement = () => ({
+  type: DECREMENT
+});
