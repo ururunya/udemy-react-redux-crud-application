4457cc25b31f3cc03d458ae238a7c793d61ae4b2
ururunya <codeururu@gmail.com>
2021-04-02 13:44:11 +0900

    update

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -26,7 +26,7 @@ async onSubmit(values) {
 }
 
     render() {
-        const { handleSubmit } = this.props;
+        const { handleSubmit, pristine, submitting } = this.props;
         return (
             <form onSubmit={handleSubmit(this.onSubmit)}>
                 <div>
@@ -37,7 +37,7 @@ async onSubmit(values) {
                 </div>
 
                 <div>
-                    <input type="submit" value="Submit" disabled={false} />
+                    <input type="submit" value="Submit" disabled={pristine || submitting} />
                     <Link to="/" >Cancel</Link>
                 </div>
             </form>
